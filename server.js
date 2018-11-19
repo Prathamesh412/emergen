eee936bdd0643bc7d40a8cc93abbcefa2b1566f8
Prathamesh412 <prathprabhu@gmail.com>
2018-11-19 20:57:47 +0530

    Images condition set for listing page

--- server.js
+++ server.js
@@ -8,7 +8,74 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 
-var User=require("./models/User");
+var User=require("./models/User"); //model imported.
+
+
+//login with passport starts
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const LocalStrategy = require('passport-local').Strategy
+
+const loginuser = {
+  username: 'test',
+  passwordHash: 'test',
+  id: 1
+}
+function findUser (username, callback) {
+    if (username === loginuser.username) {
+      return callback(null, loginuser)
+    }
+    return callback(null)
+  }
+  
+  passport.serializeUser(function (loginuser, cb) {
+    cb(null, loginuser.username)
+  })
+  
+  passport.deserializeUser(function (username, cb) {
+    findUser(username, cb)
+  })
+  
+  function initPassport () {
+    passport.use(new LocalStrategy(
+      (username, password, done) => {
+        findUser(username, (err, loginuser) => {
+          if (err) {
+            return done(err)
+          }
+  
+          // User not found
+          if (!loginuser) {
+            console.log('User not found')
+            return done(null, false)
+          }
+  
+          // Always use hashed passwords and fixed time comparison
+          bcrypt.compare(password, loginuser.passwordHash, (err, isValid) => {
+            if (err) {
+              return done(err)
+            }
+            if (!isValid) {
+              return done(null, false)
+            }
+            return done(null, loginuser)
+          })
+        })
+      }
+    ))
+}
+  
+passport.authenticationMiddleware = authenticationMiddleware
+
+function authenticationMiddleware () { // passport middleware
+    return function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next()
+      }
+      res.redirect('/')
+    }
+  }
+// login with passport ends
 
 // Multer image start
 const storage = multer.diskStorage({
@@ -40,6 +107,8 @@ app.use(express.static(__dirname + '/public'));
 //app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 // app.use(robots({UserAgent: '*', Disallow: '/'})) //robot.txt to stop crawling
 
 //****************ROUTES********************
@@ -71,11 +140,12 @@ app.get("/",function(req,res){
 
 app.get("/aryan1993/images/:page",function(req,res){
 
-    var perPage = 2;
+    var perPage = 10;
     var page = req.params.page || 1;
    // console.log(page)
     User.find({}).skip((perPage * page) - perPage)
     .limit(perPage)
+   // .sort({ 'user._id.getTimestamp()' : -1})  //sort needs to be done
     .exec(function(err,alluser){
         User.countDocuments().exec(function(err,count){
         if(err){
@@ -103,6 +173,24 @@ app.get("/aryan1993/:id",function(req,res){
     })
 });
 
+app.get("/login",function(req,res,){ 
+       res.render("login");
+});
+
+app.post('/login',function(req,res,next){
+    console.log("loginuser")
+    passport.authenticate("local", function(err, loginuser, info){
+        console.log(loginuser)
+        if(err){
+            res.redirect("/")
+        }
+        else{
+            res.redirect("/aryan1993/images/:page")
+        }
+   })
+})
+
+
 var imageName;
 var uploadFile = upload.single('image');
 app.post('/send', uploadFile, (req, res) => {
@@ -111,8 +199,15 @@ app.post('/send', uploadFile, (req, res) => {
     var email = req.body.email;
     var phone = req.body.phone;
     var message = req.body.message;
-    var image = req.file.filename;
-    console.log(req.file);
+   // console.log("The length is" + req.file.filename.length)
+   // console.log(req.file)
+    if (req.file){
+    var image = req.file.filename
+    }
+    else{
+        var image = "null"
+    }
+    console.log(image);
     //console.log(name,image);
     var newUser= {name:name,email:email,phone:phone,message:message,image:image};
 
@@ -126,7 +221,9 @@ app.post('/send', uploadFile, (req, res) => {
     })
 
     // Mailer implementation
-    var output = `
+    if (req.file){
+    var output = 
+    `
       <p>You have a new Contact Request from Energen Website</p>
       <h3>Contact Details</h3>
       <ul>  
@@ -138,8 +235,21 @@ app.post('/send', uploadFile, (req, res) => {
       <h3>Message</h3>
       <p>${req.body.message}</p>
     `;
-    // <h3>Attachment</h3>  Attachments if need to be added above in output
-    // <p>${req.body.attachment}</p>
+    }
+    else{
+        var output = 
+    `
+      <p>You have a new Contact Request from Energen Website</p>
+      <h3>Contact Details</h3>
+      <ul>  
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
+    }   //
   
     var transporter = nodemailer.createTransport(smtpTransport({
         service: 'gmail',
