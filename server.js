045213868605fd6dc86d4c500d42098fd95279cf
Prathamesh412 <prathprabhu@gmail.com>
2018-11-21 23:41:23 +0530

    Ready to run on AWS server

--- server.js
+++ server.js
@@ -107,8 +107,8 @@ app.use(express.static(__dirname + '/public'));
 //app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 // app.use(robots({UserAgent: '*', Disallow: '/'})) //robot.txt to stop crawling
 
 //****************ROUTES********************
